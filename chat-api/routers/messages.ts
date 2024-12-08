ff3b7e9c308a89c519e0af11372a986ba682118e
Nelli Gorlova <nelli7035@gmail.com>
2024-12-09 05:51:07 +0600

    Added a request for messages by date

--- chat-api/routers/messages.ts
+++ chat-api/routers/messages.ts
@@ -12,16 +12,29 @@ const messagesRouter = express.Router();
 messagesRouter.get("/",  async (req, res) => {
     try {
         const messages = await fileDb.getMessages();
+        const queryDate = req.query.dateTime as string;
+
+        if (queryDate) {
+            console.log("Received date:", queryDate);
+
+            const date = new Date(queryDate);
+            if (isNaN(date.getTime())) {
+                 res.status(400).send('Invalid date format');
+
+            } else {
+                const filteredMessages = messages.filter(msg => new Date(msg.dateTime) > date);
+                 res.status(200).send(filteredMessages.slice(-30));
+            }
+        }
+
         res.status(200).send(messages);
+
     } catch (e) {
         res.status(500).send('Error :(');
     }
 
 });
 
-messagesRouter.get("/:id", (req, res) => {
-    res.status(200).send('Страничка тута с сообщениями по дате')
-});
 
 messagesRouter.post("/", async (req, res) => {
     const { message, author } = req.body as bodyContext;
